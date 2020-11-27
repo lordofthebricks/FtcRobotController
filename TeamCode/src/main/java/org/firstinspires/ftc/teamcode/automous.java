b30c175b53624ba30bdcc7e347d86c9da6313297
victor <victorgilescummins@gmail.com>
2020-11-27 16:51:00 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automous.java
@@ -4,15 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.teamcode.vars;
-
+import static org.firstinspires.ftc.teamcode.vars.COUNTS_PER_INCH;
 @Autonomous
 public class automous extends LinearOpMode{
 
-
+    hardware robot = new hardware();
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        waitForStart();
+
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop1.java
@@ -4,18 +4,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+import org.firstinspires.ftc.teamcode.vars;
 
 @TeleOp(name = "GimliTeleopDriverControl")
 public class teleop1 extends LinearOpMode {
-    static final double     COUNTS_PER_MOTOR_REV    = 1425.2;//356.3 ;    // eg: DC Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 3.75 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            //first hundred digits of pi fr more accuracy
-
-
-            (WHEEL_DIAMETER_INCHES * 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679);
 
 
             hardware robot = new hardware();
