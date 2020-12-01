2882b6cb860173b1f8d5122009fae408d58501b7
Pengiunolo <46852384+Pengiunolo@users.noreply.github.com>
2020-12-01 18:57:44 -0500

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEstShooterTeleop.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "GimliTeleopDriverControl")
+public class TEstShooterTeleop extends LinearOpMode {
+
+
+            hardware robot = new hardware();
+            private ElapsedTime runtime = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            robot.Right_Top.setPower(gamepad1.right_stick_y);
+            robot.Right_Bottom.setPower(gamepad1.right_stick_y);
+            robot.Left_Top.setPower(gamepad1.left_stick_y);
+            robot.Left_Bottom.setPower(gamepad1.left_stick_y);
+
+
+            if (gamepad1.x){
+                robot.Shooter.setPower(0.8);
+            } else {
+                robot.Shooter.setPower(0);
+            }
+
+
+            if (gamepad1.a){
+                robot.loader.setPower(.8);
+            } else {
+                robot.loader.setPower(0);
+            }
+        }
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingHradware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingHradware.java
@@ -2,31 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class hardware {
-
-    public DcMotor Right_Bottom;
-    public DcMotor Right_Top;
-    public DcMotor Left_Bottom;
-    public DcMotor Left_Top;
+public class TestingHradware {
     public DcMotor Shooter;
     public DcMotor loader;
 
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
-    public hardware() {
+    public TestingHradware() {
 
     }
     public void init(HardwareMap ahwMap){
         hwMap = ahwMap;
-        Right_Bottom = hwMap.get(DcMotor.class, "Right_Bottom");
-        Right_Top = hwMap.get(DcMotor.class, "Right_Top");
-        Left_Bottom = hwMap.get(DcMotor.class, "Left_Bottom");
-        Left_Top = hwMap.get(DcMotor.class, "Left_Top");
         Shooter = hwMap.get(DcMotor.class,"Shooter");
         loader = hwMap.get(DcMotor.class,"Loader");
 
@@ -38,4 +27,3 @@ public class hardware {
         Shooter.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 }
-
