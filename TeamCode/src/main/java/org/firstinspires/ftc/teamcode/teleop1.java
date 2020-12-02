c0741348e0c126c7128bed962c5419c864666d4c
Pengiunolo <46852384+Pengiunolo@users.noreply.github.com>
2020-12-01 19:31:13 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop1.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.vars;
 
-@TeleOp(name = "GimliTeleopDriverControl")
+@TeleOp(name = "Normal teleop")
 public class teleop1 extends LinearOpMode {
 
 
