2e98a77d985ca9f8c6f462dbe1372281c49d60a9
Pengiunolo <46852384+Pengiunolo@users.noreply.github.com>
2021-02-25 20:27:09 -0500

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotesttleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotesttleop.java
@@ -21,16 +21,50 @@ public class Servotesttleop extends LinearOpMode {
         while (opModeIsActive()){
 
 
-            if (gamepad1.dpad_up);{
-                position = position + 0.1;
-                robot.Claw.setPosition(position);
-                telemetry.addData("position of claw:",position);
+            if (gamepad1.right_trigger == 1) {
+                robot.Claw.setPosition(0.1);
+                telemetry.addData("position:", "0.1");
                 telemetry.update();
             }
-            if (gamepad1.dpad_down); {
-                position = position - 0.1;
-                robot.Claw.setPosition(position);
-                telemetry.addData("position of claw:",position);
+
+            if (gamepad1.left_trigger == 1) {
+                robot.Claw.setPosition(0.2);
+                telemetry.addData("position:", "0.2");
+                telemetry.update();
+            }
+            if (gamepad1.left_bumper) {
+                robot.Claw.setPosition(0.3);
+                telemetry.addData("position:", "0.3");
+                telemetry.update();
+            }
+            if (gamepad1.right_bumper) {
+                robot.Claw.setPosition(0.4);
+                telemetry.addData("position:", "0.4");
+                telemetry.update();
+            }
+            if (gamepad1.b) {
+                robot.Claw.setPosition(0.5);
+                telemetry.addData("position:", "0.5");
+                telemetry.update();
+            }
+            if (gamepad1.a) {
+                robot.Claw.setPosition(0.6);
+                telemetry.addData("position:", "0.6");
+                telemetry.update();
+            }
+            if (gamepad1.x) {
+                robot.Claw.setPosition(0.7);
+                telemetry.addData("position:", "0.7");
+                telemetry.update();
+            }
+            if (gamepad1.y) {
+                robot.Claw.setPosition(0.8);
+                telemetry.addData("position:", "0.8");
+                telemetry.update();
+            }
+            if (gamepad1.left_stick_button) {
+                robot.Claw.setPosition(0.9);
+                telemetry.addData("position:", "0.9");
                 telemetry.update();
             }
 
